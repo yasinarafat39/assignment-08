3ad441cf74b282f879352d8a4584caa1a729c7a5
yasinarafat39 <yasinarafat2557@gmail.com>
2022-09-29 13:38:36 +0600

    player cart added without side bar

--- src/Components/BD-cricket-club/BdCricketClub.js
+++ src/Components/BD-cricket-club/BdCricketClub.js
@@ -4,34 +4,36 @@ import Player from '../Player/Player';
 import './BdCricketClub.css';
 
 const BdCricketClub = () => {
-    const [players, setPlayer] = useState([]);
+    const [players, setPlayers] = useState([]);
     useEffect(() => {
         fetch('players.json')
             .then(res => res.json())
-            .then(data => setPlayer(data))
+            .then(data => setPlayers(data))
     }, []);
 
     return (
-        
-        < div className = 'Bd-cricket-club' >
 
-            <div className="player-container px-12 border bg-green-100">
-                <div className='border pt-7'>
+        < div className='Bd-cricket-club' >
+
+            <div className="player-container px-12 border bg-green-50">
+                <div className='border pt-7 mb-5' >
                     <div className='flex items-center mb-1'>
                         <img src={logo} alt="" width='50px' height='70px' />
                         <h2 className='text-green-400 text-2xl font-bold'>BD CRICKET CLUB</h2>
                     </div>
                     <h3 className='text-md font-semibold'>Select today's exercise</h3>
                 </div>
-                
-                {
-                    players.map(player => <Player player={player} key={player.id}></Player>)
-                }
+
+                <div className='grid gap-4 lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2'>
+                    {
+                        players.map(player => <Player player={player} key={player.id}></Player>)
+                    }
+                </div>
             </div>
 
 
             <div className="side-bar ">
-
+                    
             </div>
         </div >
     );
--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,12 +1,23 @@
 
 import React from 'react';
 
-const Player = ({id, name, picture, age}) => {
-    console.log(name)
+const Player = (props) => {
+    const { name, id, picture, age, TimeRequired } = props.player;
     return (
-        <div>
-            <p>{name}</p>
+        <div className='singlePlayerCart rounded-lg  bg-white relative h-80'>
+
+            <img className=' rounded-lg' src={picture} alt="" />
+            <div className='px-3'>
+                <h2 className='mt-2 font-bold text-sm '>{name}</h2>
+                <p className='text-xs font-semibold'>Age: {age}</p>
+                <p className='text-xs font-semibold'>Time Required: {TimeRequired}s</p>
+
+            </div>
+            <div>
+                <button className='w-full py-1 px-1 rounded bg-green-400 text-white absolute bottom-0'>Add to list</button>
+            </div>
         </div>
+
     );
 };
 
